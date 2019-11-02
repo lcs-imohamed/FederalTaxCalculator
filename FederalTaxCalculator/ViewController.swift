7bdc063a28992b5dcbdc5fe0e78556eca3a14ef4
Ilana Mohamed <Imohamed@lcs.on.ca>
2019-11-02 14:22:19 -0400

    Added outlets and guard statements.

--- FederalTaxCalculator/ViewController.swift
+++ FederalTaxCalculator/ViewController.swift
@@ -9,12 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //MARK:Properties
+    @IBOutlet weak var personName: UITextField!
+    @IBOutlet weak var annualIncome: UILabel!
+    @IBOutlet weak var labelTaxOwed: UILabel!
+    @IBOutlet weak var effectiveTaxRate: UILabel!
     override func viewDidLoad() {
+    
+    //MARK: Methods
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    @IBAction func calculateTax(_ sender: Any) {
+        
+        //Obtain value from text field
+        guard let personNameAsString = personName.text else {
+            labelTaxOwed.text = "Please input a value. "
+            return
+        }
+        guard let annualIncomeAsString = annualIncome.text else {
+            labelTaxOwed.text = "Please input a value."
+            return
+        }
+        //Change to double
+            guard let annualIncomeAsDouble = Double(annualIncomeAsString) else {
+                labelTaxOwed.text = "Please input a numerical value."
+                return
+            }
+        //Calculate tax
+        
+        }
+    }
+   
 
 
-}
 
